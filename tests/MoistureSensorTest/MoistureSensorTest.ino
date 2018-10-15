9c6f0cd7b4cd72c9e9025558992d36d647801aaa
Elliott Indiran <eindiran@users.noreply.github.com>
2018-10-15 13:15:34 -0700

    Switch instead of giant if block

--- tests/MoistureSensorTest/MoistureSensorTest.ino
+++ tests/MoistureSensorTest/MoistureSensorTest.ino
@@ -5,11 +5,11 @@
  */
 
 // Initialize globals.
-int DELAY_SECONDS = 1; // Time between each read of the sensor in seconds
-int AIR_VALUE = 1000;  // Replace this value with sensor reading of air
-int WATER_VALUE = 200; // Replace this value with sensor reading of water
-int INTERVAL = 200;    // Replace this with (AIR_VALUE - WATER_VALUE)/4
-int DEBUG = 0;         // Set this to 1 to call get_moisture_level()
+const int DELAY_SECONDS = 1; // Time between each read of the sensor in seconds
+const int AIR_VALUE = 1000;  // Replace this value with sensor reading of air
+const int WATER_VALUE = 200; // Replace this value with sensor reading of water
+const int INTERVAL = 200;    // Replace this with (AIR_VALUE - WATER_VALUE)/4
+const int DEBUG = 0;         // Set this to 1 to call get_moisture_level()
 
 /**
  * One time setup; opens serial port and sets baud rate to 9600.
@@ -19,26 +19,50 @@ void setup() {
 }
 
 /**
- * Sets a string encoding moisture level.
+ * Sets a string encoding moisture level: This provides a range that the humidity falls into.
  */
 void get_moisture_level(int moisture_val, String &moisture_level) {
-  if (moisture_val <= WATER_VALUE) {
-    moisture_level = "Humidity: 100%";
-  } else if (moisture_val <= (WATER_VALUE + INTERVAL)) {
-    moisture_level = "Humidity: >75%";
-  } else if (moisture_val <= (WATER_VALUE + 2*INTERVAL)) {
-    moisture_level = "Humidity: >50%";
-  } else if (moisture_val <= (WATER_VALUE + 3*INTERVAL)) {
-    moisture_level = "Humidity: >25%";
-  } else if (moisture_val <= (AIR_VALUE)) {
-    moisture_level = "Humidity: >0%";
-  } else {
-    moisture_level = "Humidity: 0%";
+  switch (moisture_val) {
+    case (WATER_VALUE - 100) ... WATER_VALUE:
+      moisture_level = "Humidity: 100%";
+      break;
+    case (WATER_VALUE + 1) ... (WATER_VALUE + INTERVAL):
+      moisture_level = "Humidity: >75%";
+      break;
+    case (WATER_VALUE + INTERVAL + 1) ... (WATER_VALUE + 2 * INTERVAL):
+      moisture_level = "Humidity: >50%";
+      break;
+    case (WATER_VALUE + 2 * INTERVAL + 1) ... (WATER_VALUE + 3 * INTERVAL):
+      moisture_level = "Humidity: >25%";
+      break;
+    case (WATER_VALUE + 3 * INTERVAL + 1) ... AIR_VALUE:
+      moisture_level = "Humidity: >0%";
+      break;
+    default: // Humididty level of soil is less than humidity in air
+      moisture_level = "Humidity: 0%";
+      break;
   }
 }
 
 /**
- * Print sensor value.
+ * Get moisture as a percentage value.
+ */
+int moisture_percentage(int moisture_val) {
+  int moisture_range = AIR_VALUE - WATER_VALUE;
+  return ((moisture_val - WATER_VALUE) * 100) / moisture_range;
+}
+
+/**
+ * Sets a string encoding the moisture level: This provides an exact percentage.
+ * The sensor may drift, so this percentage can appear to "float" around.
+ */
+void get_moisture_percentage(int moisture_val, String &moisture_level) {
+  int moisture_pcent_val = moisture_percentage(moisture_val);
+  moisture_level = "Humididty: " + String(moisture_pcent_val) + "%";
+}
+
+/**
+ * Print the exact sensor value.
  */
 void print_sensor_value(int moisture_val) {
   Serial.print("Moisture Sensor Value:\t"); Serial.println(moisture_val);
