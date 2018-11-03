74953cdb82ca96ff0cd40c84d2b22577b54f3f26
Elliott Indiran <eindiran@users.noreply.github.com>
2018-11-03 14:12:46 -0700

    Moving over improvements from the tests

--- src/AutoWater/AutoWater.ino
+++ src/AutoWater/AutoWater.ino
@@ -5,19 +5,20 @@
  * Author: eindiran <eindiran@uchicago.edu>
  */
 
-// Initialize globals.
-int DEBUG = 1;
-int DELAY_SECONDS = 5;   // time between moisture checks
-int AIR_VALUE = 1000;    // Replace this value with sensor reading of air
-int WATER_VALUE = 200;   // Replace this value with sensor reading of water
-int INTERVAL = 200;      // Replace this with (AIR_VALUE - WATER_VALUE)/4
-int DRYNESS_THRESHOLD = 425;
-int SOLENOID_PIN = 7;
-int WATER_TIME_MS = 450; // time to open SV in ms
+/** Initialize globals */
+const int DELAY_SECONDS = 5;   // Time between moisture checks
+const int AIR_VALUE = 1000;    // Replace this value with sensor reading of air
+const int WATER_VALUE = 200;   // Replace this value with sensor reading of water
+const int INTERVAL = 200;      // Replace this with (AIR_VALUE - WATER_VALUE)/4
+const int DRY_THRESH = 425;    // The moisture value beneath which you water
+const int SOLENOID_PIN = 7;    // Arduino pin tied to the SV
+const int WATER_TIME_MS = 450; // Time to open the SV in ms
+const int DEBUG = 1;           // 0,1,2 toggle verboseness off-low-high
+                               // 3 and > turn off watering, but preserve verboseness
 
 /**
- * One-time setup:
- * open up the serial port, set he baud rate to 9600 and prep the SV's pin.
+ * setup() --> One-time setup tasks go here.
+ * Opening up the serial port, set the baud rate to 9600 and prep the SV's pin.
  */
 void setup() {
   Serial.begin(9600);
@@ -25,27 +26,59 @@ void setup() {
 }
 
 /**
- * Sets a string encoding moisture level.
+ * get_moisture_level() --> Sets a string encoding moisture level.
+ * Uses ranges / buckets to define moisture level. These buckets
+ * allow you to determine to a coarse degree how moist it is.
  */
 void get_moisture_level(int moisture_val, String &moisture_level) {
-  if (moisture_val <= WATER_VALUE) {
-    moisture_level = "Humidity: 100%";
-  } else if (moisture_val <= (WATER_VALUE + INTERVAL)) {
-    moisture_level = "Humidity: >75%";
-  } else if (moisture_val <= (WATER_VALUE + 2*INTERVAL)) {
-    moisture_level = "Humidity: >50%";
-  } else if (moisture_val <= (WATER_VALUE + 3*INTERVAL)) {
-    moisture_level = "Humidity: >25%";
-  } else if (moisture_val <= (AIR_VALUE)) {
-    moisture_level = "Humidity: >0%";
-  } else {
-    moisture_level = "Humidity: 0%";
+  switch (moisture_val) {
+    // This special form of `case` syntax is supported by gcc and therefore by Arduino
+    // It allows you to specify a range; `case A ... B` is true whenever the value being
+    // switched on is between A and B.
+    case (WATER_VALUE - 100) ... WATER_VALUE:
+      moisture_level = "Humidity: 100%";
+      break;
+    case (WATER_VALUE + 1) ... (WATER_VALUE + INTERVAL):
+      moisture_level = "Humidity: >75%";
+      break;
+    case (WATER_VALUE + INTERVAL + 1) ... (WATER_VALUE + 2 * INTERVAL):
+      moisture_level = "Humidity: >50%";
+      break;
+    case (WATER_VALUE + 2 * INTERVAL + 1) ... (WATER_VALUE + 3 * INTERVAL):
+      moisture_level = "Humidity: >25%";
+      break;
+    case (WATER_VALUE + 3 * INTERVAL + 1) ... AIR_VALUE:
+      moisture_level = "Humidity: >0%";
+      break;
+    default: // Humididty level of soil is less than humidity in air
+      moisture_level = "Humidity: 0%";
+      break;
   }
 }
 
 /**
- * Puts a normally-closed solenoid valve into its open
- * state for num_seconds, then closes it.
+ * moisture_percentage() --> Returns an int representing the humidity
+ * percentage. It is calculated based on the range of the baseline values
+ * for AIR_VALUE and WATER_VALUE.
+ */
+int moisture_percentage(int moisture_val) {
+  int moisture_range = AIR_VALUE - WATER_VALUE;
+  return ((moisture_val - WATER_VALUE) * 100) / moisture_range;
+}
+
+/**
+ * get_moisture_percentage() -->  sets a string encoding the moisture level 
+ * as an exact percentage. The sensor may drift, so this percentage can appear to
+ * "float" around.
+ */
+void get_moisture_percentage(int moisture_val, String &moisture_level) {
+  int moisture_pcent_val = moisture_percentage(moisture_val);
+  moisture_level = "Humididty: " + String(moisture_pcent_val) + "%";
+}
+
+/**
+ * nc_turn_solenoid() --> Puts a normally-closed solenoid valve
+ * into its open state for num_seconds, then closes it.
  */
 void nc_turn_solenoid(int num_seconds) {
   int num_ms = num_seconds * 1000;
@@ -61,10 +94,10 @@ void nc_turn_solenoid(int num_seconds) {
 }
 
 /**
- * Water our plant based on the moisture sensor reading.
+ * water_plant() --> Water the plant based on the moisture sensor readings.
  */
 void water_plant(int moisture_val) {
-  if (moisture_val > DRYNESS_THRESHOLD) {
+  if (moisture_val > DRY_THRESH) {
     if (DEBUG) {
       Serial.println("Moisture beneath threshold: watering plant.");
     }
@@ -75,31 +108,42 @@ void water_plant(int moisture_val) {
 }
 
 /**
- * Print sensor value.
+ * print_sensor_value() --> Print the exact sensor value. Takes a single
+ * int as input.
  */
 void print_sensor_value(int val) {
   Serial.print("Moisture Sensor Value:\t"); Serial.println(val);
 }
 
 /**
- * Main loop.
+ * loop() --> Main loop of the program.
+ * (1) Read moisture sensor.
+ * (2) Print out information about the sensor if necessary.
+ * (3) If the moisture level is beneath the dryness threshold,
+ *     we now try watering the plant via water_plant().
+ * (3) Wait a reasonable period of time before checking the sensor
+ *     again.
  */
 void loop() {
   int moisture_val;             // reading from moisture sensor
   moisture_val = analogRead(0); // connect sensor to Analog 0
   print_sensor_value(moisture_val);
-  if (DEBUG) {
-    // Print human-readable moisture level
+  if (DEBUG == 1) {
+    // Print human-readable moisture levels, as buckets
     String moisture_level;
     get_moisture_level(moisture_val, moisture_level);
     Serial.println(moisture_level);
-  }
-  if (DEBUG == 2) {
-    // Check sensor without actually activating SV
+  } else if (DEBUG > 1) {
+    // Print human-readable moisture levels, as percentages
+    String moisture_level;
+    get_moisture_percentage(moisture_val, moisture_level);
+    Serial.println(moisture_level);
+  } else if (DEBUG > 2) {
+    // Check sensor, without actually activating the SV
     delay(DELAY_SECONDS * 1000);
     return;
   }
+  // If your DEBUG was > 2, you expect to skip this step.
   water_plant(moisture_val);
   delay(DELAY_SECONDS * 1000);
 }
-
